b4f4405fb2a938f84c2e75b1241555de67fca7a7
pilip.yurchanka <p.yurchanka@mobidev.biz>
2026-01-02 22:29:24 +0100

    Qt build restored

--- modules/platform/qt/src/main/java/org/example/demo/QtMain.java
+++ modules/platform/qt/src/main/java/org/example/demo/QtMain.java
@@ -17,4 +17,8 @@ public class QtMain {
         log.debug(INITIALIZATION_MARKER, "Start application with args:{}", List.of(args));
         new QtAppInitializer().initialize(args);
     }
+
+    public static void init(final String[] args) {
+        main(args);
+    }
 }
